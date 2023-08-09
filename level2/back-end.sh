d7030fe04ce4f74b6ac3eea4d5a4c1d9124748df
Hung Pham <pham.quang.thanh.hung@gmail.com>
2023-08-09 19:31:11 +0300

    update backend bash script

--- level2/back-end.sh
+++ level2/back-end.sh
@@ -1,6 +1,11 @@
 sudo apt update -y
-git clone https://github.com/vladimirmukhin/instagram-mern.git
-mv /root/instagram-mern/backend/config/config.env.example /root/instagram-mern/backend/config/config.env
-#edit conf.enf file
 sudo apt install npm -y
 
+git clone https://github.com/vladimirmukhin/instagram-mern.git
+mv /root/instagram-mern/backend/config/config.env.example /root/instagram-mern/backend/config/config.env
+
+#edit conf.enf file
+
+cd /root/instagram-mern
+npm install
+npm start
