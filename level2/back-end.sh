afc94fa82ff718f3e24a3596cb74e710e038a322
Hung Pham <pham.quang.thanh.hung@gmail.com>
2023-08-10 18:43:03 +0300

    update bash script

--- level2/back-end.sh
+++ level2/back-end.sh
@@ -2,11 +2,12 @@
 sudo apt update -y
 sudo apt install npm -y
 sudo apt install git -y
-sudo git clone https://github.com/vladimirmukhin/instagram-mern.git
-mv /root/instagram-mern/backend/config/config.env.example /root/instagram-mern/backend/config/config.env
+sudo git clone https://github.com/vladimirmukhin/instagram-mern.git /home/ubuntu/instagram-mern
+
+mv /home/ubuntu/instagram-mern/backend/config/config.env.example /home/ubuntu/instagram-mern/backend/config/config.env
 
 #edit conf.enf file
 
-cd /root/instagram-mern
-npm install
-npm start
+# cd /home/ubuntu/instagram-mern
+# npm install
+# npm start
--- level2/front-end.sh
+++ level2/front-end.sh
@@ -3,27 +3,27 @@ sudo apt update -y
 sudo apt install npm -y
 sudo apt install nginx -y
 sudo apt install git -y
-# still get the problem when the repository code cannot be downloaded
-sudo git clone https://github.com/vladimirmukhin/instagram-mern.git
 
-rm -rf /root/instagram-mern/backend
-cd /root/instagram-mern/frontend
+sudo git clone https://github.com/vladimirmukhin/instagram-mern.git /var/tmp/instagram-mern
+
+
+rm -rf /var/tmp/instagram-mern/backend
+cd /var/tmp/instagram-mern/frontend
 mkdir server_logs
 npm install # download all dependencies
 npm run build
 
 mkdir /var/www/hungpham.link
-cp -r /root/instagram-mern/* /var/www/hungpham.link/
+cp -r /var/tmp/instagram-mern/* /var/www/hungpham.link/
 
 sudo chown -R www-data.www-data /var/www/hungpham.link/
 sudo chmod -R 755 /var/www/hungpham.link/
 
 rm -f /etc/nginx/nginx.conf
-wget -0 nginx.conf https://github.com/hungphamLTC/MERN-stack-deployment-on-AWS/blob/mern-stack-deployment/server-configuration/nginx.conf
-sudo mv nginx.conf /etc/nginx/
+sudo wget -O /etc/nginx/nginx.conf https://github.com/hungphamLTC/MERN-stack-deployment-on-AWS/raw/mern-stack-deployment/server-configuration/nginx.conf
 
-wget -O default.conf https://raw.githubusercontent.com/hungphamLTC/MERN-stack-deployment-on-AWS/mern-stack-deployment/server-configuration/default.conf
-sudo mv default.conf /etc/nginx/conf.d/
+wget -O /etc/nginx/conf.d/default.conf https://raw.githubusercontent.com/hungphamLTC/MERN-stack-deployment-on-AWS/mern-stack-deployment/server-configuration/default.conf
+
 
 sudo nginx -t # check the syntax
 sudo service nginx restart
